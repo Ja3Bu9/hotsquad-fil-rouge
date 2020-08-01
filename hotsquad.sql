83d2ec8ce987e74793a1304751e976e1be1099d6
Ja3Bu9 <ab.banana@hotmail.fr>
2020-08-01 01:15:41 +0100

    DB

---
+++ hotsquad.sql
@@ -0,0 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2020 at 02:15 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotsquad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `photo`, `name`) VALUES
+(1, 'pubg.jpg', 'PUBG'),
+(3, 'fifa20.png', 'FIFA 20'),
+(4, 'fortnite.jpg', 'Fortnite'),
+(5, 'gta5.jpg', 'GTA V'),
+(6, 'leagueoflegends.jpg', 'League of Legends'),
+(7, 'minecraft.png', 'Minecraft'),
+(8, 'rainbowsix.jpg', 'R6S'),
+(10, 'valorant.jpg', 'Valorant'),
+(11, 'codwarzone.jpg', 'COD:Warzone');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `upvotes` int(11) NOT NULL,
+  `downvotes` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `report` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`id`, `title`, `content`, `photo`, `upvotes`, `downvotes`, `date`, `report`, `user_id`, `cat_id`) VALUES
+(30, 'sasasasa', 'fafafafa', 'Icon_jerrycan.png', 2, 0, '2020-07-27', 1, 9, 3),
+(32, 'loremmmm', 'dzdzdzdzdzdzdzdzdz', 'back.PNG', 2, 0, '2020-08-01', 0, 9, 3),
+(33, 'dadad', 'FFFFFFFFFFFFFFFFFFFFFFFFFFFF', 'brand.png', 2, 0, '2020-08-01', 0, 9, 4),
+(34, 'NICE', 'Amazingggg', 'the-witcher-henry-cavill-4k-new-8s.jpg', 1, 0, '2020-08-01', 0, 12, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `date`, `firstname`, `lastname`, `photo`, `role`) VALUES
+(9, 'Ja3Bu9', '$2y$10$Us3KpXMcstXhW66IsUuRnOe99xQ0CDsSJ9qvs4zd/H7c7VxbCEo9G', 'hababasami@gmail.com', '2020-07-20', 'Sami', 'Hababa', 'face1.PNG', 'admin'),
+(12, 'test', '$2y$10$We9k2mjho5ktxU.YZWNWzOSJNVqQv6nBJ89oq8CY7mcaFWH3MMATi', 'test@test.com', '2020-07-29', 'test', 'test', 'user.jpg', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voted`
+--
+
+CREATE TABLE `voted` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `up` tinyint(1) NOT NULL,
+  `down` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `voted`
+--
+
+INSERT INTO `voted` (`id`, `post_id`, `user_id`, `up`, `down`) VALUES
+(16, 27, 9, 1, 0),
+(19, 31, 9, 1, 0),
+(20, 30, 9, 1, 0),
+(21, 25, 9, 1, 0),
+(22, 33, 9, 1, 0),
+(23, 32, 9, 1, 0),
+(24, 33, 12, 1, 0),
+(25, 32, 12, 1, 0),
+(26, 30, 12, 1, 0),
+(27, 34, 12, 1, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `voted`
+--
+ALTER TABLE `voted`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `voted`
+--
+ALTER TABLE `voted`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`cat_id`) REFERENCES `category` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
